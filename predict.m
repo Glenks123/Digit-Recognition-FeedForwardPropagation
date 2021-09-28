0c0a4c603f2f0070cffe040c7cac187f32397c86
Glenks123 <63196484+Glenks123@users.noreply.github.com>
2021-09-28 17:35:03 +0300

    Update predict.m

--- predict.m
+++ predict.m
@@ -1,13 +1,8 @@
 function p = predict(Theta1, Theta2, X)
-%PREDICT Predict the label of an input given a trained neural network
-%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
-%   trained weights of a neural network (Theta1, Theta2)
 
-% Useful values
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
 X = [ones(m, 1), X];
@@ -19,10 +14,9 @@ z2 = a1*Theta1';
 a2 = [ones(m, 1), sigmoid(z2)];
 
 z3 = a2*Theta2';
-% a3 is a vector that contains the probability for each label.
+% a3 is our output layer that contains the probability for each label.
 a3 = sigmoid(z3);
 
-% returns the maximum value (highest prediction) from the output layer a3
 [maxval, indices] = max(a3, [], 2);
 p = indices;
 
